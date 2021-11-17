2321bfe63fc440ab46921d3878cf7be28b1a35bc
irfanniaz <irfanniaz7@gmail.com>
2021-11-17 00:28:07 -0800

    Demo work

---
+++ src/Http/Controllers/ContactController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Irfan\Contact\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Irfan\Contact\Models\Contact;
+use Irfan\Contact\mail\ContactMailable;
+use Illuminate\Support\Facades\Mail;
+
+
+class ContactController extends Controller
+{
+    
+    public function index()
+    {
+    return view('contact::contact');
+
+    }
+
+    public function send(Request $request)
+    {
+        Mail::to(config('contact.send_email_to'))->send(new ContactMailable($request->message,$request->name)); 
+        Contact::create($request->all());   
+        return redirect(route('contact'));
+
+    }
+}
---
+++ src/config/contact.php
@@ -0,0 +1,4 @@
+<?php
+return [
+    'send_email_to' =>"irfanniaz7@gmail.com"
+];
